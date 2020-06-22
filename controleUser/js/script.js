ccbeb50b1886e003ef8ee543ad94d9aa0932c134
Vinicio D. Brejinski <48477457+ViniDB27@users.noreply.github.com>
2020-06-22 11:42:33 -0300

    exercicio

---
+++ controleUser/js/script.js
@@ -0,0 +1,15 @@
+var name = document.querySelector('#exampleInputName')
+var gender = document.querySelectorAll('#form-user-create [name-gender]:checked')
+var birth = document.querySelector('#exampleInputBirth')
+var country = document.querySelector('#exampleInputCountry')
+var email = document.querySelector('#exampleInputEmail')
+var password = document.querySelector('#exampleInputPassword')
+var file = document.querySelector('#exampleInputFile')
+var adm = document.querySelector('#admin')
+
+var fields = document.querySelectorAll('#form-user-create [name]')
+fields.forEach((field, index)=>{
+
+    
+
+})
